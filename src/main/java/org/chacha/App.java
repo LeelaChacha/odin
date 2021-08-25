0210ded56541ed335b84ab424cea4694f2f4874a
Chacha <hukumrajsinghdeora@gmail.com>
2021-08-26 01:38:17 +0200

    Initial commit

---
+++ src/main/java/org/chacha/App.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2021 Hukumraj Singh Deora
+ */
+
+package org.chacha;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/chacha/AppTest.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2021 Hukumraj Singh Deora
+ */
+
+package org.chacha;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertThat(true).isTrue();
+    }
+}
