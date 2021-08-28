1f972a523f23f162949304be94c5c02c2022c222
Chacha <hukumrajsinghdeora@gmail.com>
2021-08-28 03:04:26 +0200

    removes Thread.sleep() from Unit test for more robustibility

--- src/test/java/org/chacha/odin/PollingSchedulerTest.java
+++ src/test/java/org/chacha/odin/PollingSchedulerTest.java
@@ -7,9 +7,11 @@ package org.chacha.odin;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 
 class PollingSchedulerTest {
     private IPollingScheduler pollingScheduler;
@@ -25,7 +27,9 @@ class PollingSchedulerTest {
         counter.set(0);
 
         pollingScheduler.startScheduling(counter::getAndIncrement, 5);
-        Thread.sleep(12000);
+        await().atLeast(9, TimeUnit.SECONDS)
+                .atMost(15, TimeUnit.SECONDS)
+                .until(() -> {return counter.get() == 2;});
         pollingScheduler.stopScheduling();
 
         assertThat(counter.get()).isEqualTo(2);
