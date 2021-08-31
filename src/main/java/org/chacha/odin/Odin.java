b830138bf1fa3c100e7f3808c919d01b9f3a690d
Chacha <hukumrajsinghdeora@gmail.com>
2021-08-31 02:46:22 +0200

    adds javadoc to the public API

--- src/main/java/org/chacha/odin/Odin.java
+++ src/main/java/org/chacha/odin/Odin.java
@@ -20,23 +20,34 @@ public class Odin {
 
     private IDatabaseIntermediate databaseIntermediate;
 
-    private final int schedulerThreadPoolSize;
-    private final IPollingScheduler pollingScheduler;
+    private int schedulerThreadPoolSize;
+    private IPollingScheduler pollingScheduler;
 
-    public Odin() throws IOException {
-        Configuration odinConfiguration = new Configuration();
-
+    /**
+     * An Odin instance will be created by this function.
+     * This instance will be configured by the application.properties file.
+     * The required properties are:
+     * <ul>
+     *     <li>odin.subscriberName: [String] Name of the your application</li>
+     *     <li>odin.tagsToMonitor: [Comma-seperated String List] With tags your application will be able to pull the records of</li>
+     *     <li>odin.connectionString and odin.databaseName: Data to connect to a MongoDB Database</li>
+     * </ul>
+     *
+     */
+    public Odin() {
         try {
+            Configuration odinConfiguration = new Configuration();
             this.subscriberName = odinConfiguration.getSubscriberName();
             this.listOfTagsToMonitor = odinConfiguration.getListOfTagsToMonitor();
+
+            this.schedulerThreadPoolSize = odinConfiguration.getPollingSchedulerThreadPoolSize();
+            this.pollingScheduler = new PollingScheduler(this.schedulerThreadPoolSize);
             this.databaseIntermediate = odinConfiguration.getDatabaseIntermediate();
         }
-        catch (Configuration.MissingPropertyException e) {
+        catch (Configuration.MissingPropertyException | IOException e) {
             logger.error(e.getMessage(), e);
+            return;
         }
-
-        this.schedulerThreadPoolSize = odinConfiguration.getPollingSchedulerThreadPoolSize();
-        this.pollingScheduler = new PollingScheduler(this.schedulerThreadPoolSize);
 
         this.subscribeToTagsIfNotAlready();
     }
@@ -55,16 +66,30 @@ public class Odin {
         this.subscribeToTagsIfNotAlready();
     }
 
+    /**
+     * @param callback This is the method that will be called when a record is pulled.
+     *                 You can give a lambda expression with two inputs (String tag, String data).
+     * @param intervalInSeconds Odin will check for new records periodically. You can decide how frequent you want to look for records.
+     */
     public void startPollingRecords(BiConsumer<String, String> callback, int intervalInSeconds){
         logger.info("Starting to poll for records with {}s interval.", intervalInSeconds);
         pollingScheduler.startScheduling(() -> pullRecords(callback), intervalInSeconds);
     }
 
+    /**
+     * This method stops polling for records. You will have to provide the callback again the next time you start polling.
+     */
     public void stopPollingRecords(){
         logger.info("Stopping to poll for records.");
         pollingScheduler.stopScheduling();
     }
 
+    /**
+     * @param callback This is the method that will be called when a record is pulled.
+     *                 You can give a lambda expression with two inputs (String tag, String data).
+     *                 The records will be pulled in a LIFO manner.
+     * This function only looks for records once. If you want to look for records periodically use {@link org.chacha.odin.Odin#startPollingRecords(BiConsumer, int)}
+     */
     public void pullRecords(BiConsumer<String, String> callback){
         logger.info("Pulling all Records.");
         for (String tag : listOfTagsToMonitor){
@@ -77,6 +102,10 @@ public class Odin {
         databaseIntermediate.deleteAllReadRecords();
     }
 
+    /**
+     * @param tag Tag that goes along with the record. Applications that configured this tag in their Odin can pull this record.
+     * @param data Data that belong with the tag. It can also be a JsonString (not parsed by Odin, however)
+     */
     public void pushRecord(String tag, String data){
         logger.info("Pushing record with Tag: {}", tag);
         databaseIntermediate.pushRecord(tag, data);
